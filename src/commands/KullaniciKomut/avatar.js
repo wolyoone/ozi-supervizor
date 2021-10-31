b76692b4e9f4715879c8584cf178c3ceb638e350
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-11-01 01:50:40 +0300

    Update avatar.js

--- src/commands/KullaniciKomut/avatar.js
+++ src/commands/KullaniciKomut/avatar.js
@@ -9,7 +9,7 @@ module.exports = {
 run: async (client, message, args, embed, prefix) => {
 	if (!message.guild) return;
 let user = args.length > 0 ? message.mentions.users.first() || await client.users.fetch(args[0]) || message.author : message.author
-message.channel.send(`${user.tag} ${user.displayAvatarURL({ dynamic: true, size: 4096 })}`)
+message.channel.send(`${user.tag} ${user.displayAvatarURL({ dynamic: true, size: 4096 })}`).then(x=>x.delete({timeout:5000}))
 
 },
   };
