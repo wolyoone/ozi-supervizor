2e60144a06e31f9b5b7a0636344290f433ac0419
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-11-04 01:44:31 +0300

    Update clickMenu2.js

--- src/events/clickMenu2.js
+++ src/events/clickMenu2.js
@@ -60,27 +60,20 @@ module.exports = async ( menu, message, args ) => {
 ////////////////////////////////////////////////////////////////////////////////////////////
 
     if (menu.values[0] === "I") {
-        setTimeout(() => { 
         menu.reply.edit(`Sunucuya Katılma Tarihiniz :  \`${moment(menu.clicker.member.joinedAt).format('D/MMMM/YYYY')}\``)
-    },3000)  
     }
 
     if (menu.values[0] === "II") {
-        setTimeout(() => { 
         menu.reply.edit(`Üzerinde Bulunan Rollerin Listesi ;
         
 ${(menu.clicker.member.roles.cache.filter(a => a.name !== '@everyone').map(a => a).join(' ') ? menu.clicker.member.roles.cache.filter(a => a.name !== '@everyone').map(a => a).join(', ') : 'Hiç yok.')}`)
-    },3000)  
     }
 
     if (menu.values[0] === "III") {
-        setTimeout(() => { 
         menu.reply.edit(`Hesabınızın Açılış Tarihi :  \`${moment(menu.clicker.member.user.createdAt).format("LLL")}\``)
-    },3000)  
     }
 
     if (menu.values[0] === "IV") {
-        setTimeout(() => { 
         menu.reply.edit(`
 ${menu.clicker.member.toString()}, üyesinin \`${moment(Date.now() + (1000*60*60*3)).format("LLL")}\` tarihinden  itibaren \`Revulion ✬ #MUTLUYARINLAR\` sunucusunda toplam invite bilgileri aşağıda belirtilmiştir.
 Toplam **${regular}** davet.
@@ -89,26 +82,21 @@ Toplam **${regular}** davet.
       
 <a:tik:899680497427431424> \`Günlük: ${daily}, Haftalık: ${weekly}, Taglı: ${tagged}\`
 `)
-    },3000)  
     }
 
     if (menu.values[0] === "V") {
         await member.roles.set(ayarlar.unregRoles);
 
-        setTimeout(() => { 
         menu.reply.edit(`${menu.clicker.member.toString()} üyesi başarıyla kayıtsıza atıldı!`)
-    },3000)  
     }
 
     if (menu.values[0] === "VI") {
-        setTimeout(() => { 
         menu.reply.edit(`
 <:miniicon:899339236724068372> Sesli kanallardaki üye sayısı : \`${(menu.guild.members.cache.filter((x) => x.voice.channel).size)}\`
 <:miniicon:899339236724068372> Sunucudaki toplam üye sayısı : \`${(menu.guild.memberCount)}\`
 <:miniicon:899339236724068372> Sunucunun oluşturulma tarihi: \`${moment(menu.guild.createdAt).locale("tr").format("LLL")}\`
 <:miniicon:899339236724068372> Sunucu destek numarası : \`${(menu.guild.id)}\`
 `)
-    },3000)  
     }
 
     if (menu.values[0] === "VII") {
@@ -118,13 +106,10 @@ Toplam **${regular}** davet.
      .setTitle(`${member.user.username} üyesinin isim bilgileri;`)
      .setDescription(data ? data.names.splice(0, 10).map((x, i) => `\`${i + 1}.\` \`${x.name}\` (${x.rol}) , (<@${x.yetkili}>) , **[**\`${moment(x.date).format("LLL")}\`**]**`).join("\n") : "")
               
-        setTimeout(() => { 
         menu.reply.edit(ambed,true)
-    },3000)  
     }
 
     if (menu.values[0] === "VIII") {
-        setTimeout(() => { 
         menu.reply.edit(`
 ${menu.clicker.member.toString()}, üyesinin \`${moment(Date.now() + (1000*60*60*3)).format("LLL")}\` tarihinden  itibaren \`Revulion ✬ #MUTLUYARINLAR\` sunucusunda toplam mesaj bilgileri aşağıda belirtilmiştir.
 
@@ -134,11 +119,9 @@ ${menu.clicker.member.toString()}, üyesinin \`${moment(Date.now() + (1000*60*60
 <:tik:899339236724068372> Haftalık Mesaj: \`${Number(messageWeekly).toLocaleString()} mesaj\`
 <:tik:899339236724068372> Günlük Mesaj: \`${Number(messageDaily).toLocaleString()} mesaj\`
 `)
-    },3000)  
     }
 
     if (menu.values[0] === "IX") {
-setTimeout(() => {
 menu.reply.edit(`
 ${menu.clicker.member.toString()}, üyesinin \`${moment(Date.now() + (1000*60*60*3)).format("LLL")}\` tarihinden  itibaren \`Revulion ✬ #MUTLUYARINLAR\` sunucusunda toplam ses bilgileri aşağıda belirtilmiştir.
 
@@ -149,7 +132,6 @@ ${menu.clicker.member.toString()}, üyesinin \`${moment(Date.now() + (1000*60*60
 <:tik:899339236724068372> Günlük Ses: \`${voiceDaily}\`
 `,true);
 
-    },3000)  
     }
 
 }
