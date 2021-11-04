c13f4ac93c2095d6f39c1cf494bc9706da769b34
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-11-04 19:03:59 +0300

    Update guildMemberRemove.js

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -17,15 +17,12 @@ module.exports = async (member) => {
     await inviterSchema.findOneAndUpdate({ guildID: member.guild.id, userID: inviter.id }, { $inc: { leave: 1, total: -1 } }, { upsert: true });
     const inviterData = await inviterSchema.findOne({ guildID: member.guild.id, userID: inviter.id, });
     const total = inviterData ? inviterData.total : 0;
+    const gorevData = await gorev.findOne({ guildID: member.guild.id, userID: inviter.id });
     channel.wsend(`\`${member.user.tag}\` sunucumuzdan ayrıldı. ${inviter.tag} tarafından davet edilmişti. (**${total}** davet)`);
     await coin.findOneAndUpdate({ guildID: member.guild.id, userID: inviter.id }, { $inc: { coin: -15 } }, { upsert: true });
+    if (gorevData)
+    await gorev.findOneAndUpdate({ guildID: member.guild.id, userID: inviter.id }, { $inc: { invite: -1 } }, { upsert: true });
   }
-  const gorevData = await gorev.findOne({ guildID: member.guild.id, userID: invite.inviter.id });
-if (gorevData)
-{
-await gorev.findOneAndUpdate({ guildID: member.guild.id, userID: invite.inviter.id }, { $inc: { invite: -1 } }, { upsert: true });
-}
-
 };
 
 module.exports.conf = {
