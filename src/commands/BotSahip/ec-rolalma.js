b1c23c83fca89f5c7aba2c7429e42ba606e7d612
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-11-06 21:57:03 +0300

    Update ec-rolalma.js

--- src/commands/BotSahip/ec-rolalma.js
+++ src/commands/BotSahip/ec-rolalma.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   run: async (client, message, args) => {
-    client.api.channels(message.channel.id).messages.post({ data: {"content":`Merhaba **Revulion** üyeleri,\nÇekiliş katılımcısı alarak ${ozinitro} , ${ozispotify} , ${ozinetflix} , ${ozinexxen} , ${oziblutv} gibi çeşitli ödüllerin sahibi olabilirsiniz.\nEtkinlik katılımcısı alarak çeşitli etkinliklerin yapıldığı anlarda herkesten önce haberdar olabilirsiniz ve çekilişlere önceden katılma hakkı kazanabilirsiniz.\n\n__Aşağıda ki butonlara basarak siz de bu ödülleri kazanmaya hemen başlayabilirsiniz!__`,"components":[{"type":1,"components":[
+    client.api.channels(message.channel.id).messages.post({ data: {"content":`Merhaba **Revulion** üyeleri,\nÇekiliş katılımcısı alarak ${ozinitro} , ${ozispotify} , ${ozinetflix} , ${oziexxen} , ${oziblutv} gibi çeşitli ödüllerin sahibi olabilirsiniz.\nEtkinlik katılımcısı alarak çeşitli etkinliklerin yapıldığı anlarda herkesten önce haberdar olabilirsiniz ve çekilişlere önceden katılma hakkı kazanabilirsiniz.\n\n__Aşağıda ki butonlara basarak siz de bu ödülleri kazanmaya hemen başlayabilirsiniz!__`,"components":[{"type":1,"components":[
 
         {"type":2,"style":3,"custom_id":"buttoncekilis","label":"🎁 Çekiliş Katılımcısı"},
         {"type":2,"style":4,"custom_id":"buttonetkinlik","label":"🎉 Etkinlik Katılımcısı"}
