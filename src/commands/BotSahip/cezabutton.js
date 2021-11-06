9c096fd5df44fe875e628d469712b2c4e1ae37a3
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-11-06 14:39:00 +0300

    Update cezabutton.js

--- src/commands/BotSahip/cezabutton.js
+++ src/commands/BotSahip/cezabutton.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const conf = require("../../configs/sunucuayar.json");
+const { Jail } = require("../../configs/emojis.json")
 
 module.exports = {
   conf: {
@@ -9,7 +10,7 @@ module.exports = {
   },
 
   run: async (client, message, args) => {
-    client.api.channels(message.channel.id).messages.post({ data: {"content":"<:jail:903566151727087686> Aşağıda ki düğmelerden ceza listenizi, ceza puanını ve aktif cezanızın kalan süresini görüntüleyebilirsiniz.","components":[{"type":1,"components":[
+    client.api.channels(message.channel.id).messages.post({ data: {"content":`${Jail} Aşağıda ki düğmelerden ceza listenizi, ceza puanını ve aktif cezanızın kalan süresini görüntüleyebilirsiniz.`,"components":[{"type":1,"components":[
 
         {"type":2,"style":2,"custom_id":"cezapuan","label":"Ceza Puanı","emoji": { "id": "903565442206036019"}},
         {"type":2,"style":3,"custom_id":"cezalarım","label":"Cezalarım","emoji": { "id": "903564842978402304"}},
