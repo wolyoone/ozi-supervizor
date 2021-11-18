5cd7a91b8dd555ef06a53d991f14742faecd5eac
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-11-18 22:26:18 +0300

    Star Atmayı Unutma

--- src/commands/YetkiliKomutlari/seslisay.js
+++ src/commands/YetkiliKomutlari/seslisay.js
@@ -1,5 +1,5 @@
 const conf = require("../../configs/sunucuayar.json")
-const { red, green} = require("../../configs/emojis.json")
+const { red, green, star, Revuu, kirmiziok } = require("../../configs/emojis.json")
 module.exports = {
   conf: {
     aliases: ["seslisay","sesli"],
@@ -13,12 +13,13 @@ module.exports = {
   message.react(red)  
   return
 }
+    let members = message.guild.members.cache.filter(m => m.voice.channelID);
+
     let tag = conf.tag;
     let pubID = conf.publicParents;
 
     let topses = message.guild.members.cache.filter(s => s.voice.channel);
     let tagses = topses.filter(s => s.user.username.includes(tag));
-    let pubses = message.guild.channels.cache.filter(x => x.parentID == "899273635427135531" && x.type == "voice");
 
     let yayın = topses.filter(s => s.voice.streaming);
     let mik = topses.filter(s => s.voice.selfMute).size;
@@ -27,21 +28,21 @@ module.exports = {
     let count = 1;
     let topCategory = message.guild.channels.cache.filter(s => s.type === 'category').sort((a, b) => Number(message.guild.members.cache.filter(s => s.voice.channel && s.voice.channel.parentID === b.id).size - Number(message.guild.members.cache.filter(s => s.voice.channel && s.voice.channel.parentID === a.id).size))).map((c, index) => `${count++}. \`#${c.name}\`: \`${c.members.filter(s => s.voice.channel && s.voice.channel.parentID === c.id).size}\``).splice(0, 3).join('\n');
     let yetkili = message.guild.members.cache.filter(x => {
-      return x.user.username.includes("✬") && x.voice.channel && x.roles.cache.has("899273632637919234")
+      return x.user.username.includes(conf.tag) && x.voice.channel && x.roles.cache.has(conf.ekipRolu)
   }).size
 
     embed.setDescription(`
-<a:yildizkirmizi:899680497427431424> Sesli kanallarda toplam **${topses.size}** kişi var !
+${star} Sesli kanallarda toplam **${topses.size}** kişi var !
 
-<a:kirmiziok:901441275381817426> Public odalarda **${pubses.size}** kişi var !
-<a:kirmiziok:901441275381817426> Ses kanallarında **${tagses.size}** taglı kullanıcı var !
-<a:kirmiziok:901441275381817426> Ses kanallarında **${yetkili}** yetkili var !
+${kirmiziok} Public odalarda **${members.filter(m => pubID.includes(m.voice.channel.parentID)).size}** kişi var !
+${kirmiziok} Ses kanallarında **${tagses.size}** taglı kullanıcı var !
+${kirmiziok} Ses kanallarında **${yetkili}** yetkili var !
 
-<a:kirmiziok:901441275381817426> Ses kanallarında **${yayın.size}** kişi yayın yapıyor !
-<a:kirmiziok:901441275381817426> Mikrofonu kapalı: **${mik}**
-<a:kirmiziok:901441275381817426> Kulaklığı kapalı: **${kulak}**
+${kirmiziok} Ses kanallarında **${yayın.size}** kişi yayın yapıyor !
+${kirmiziok} Mikrofonu kapalı: **${mik}**
+${kirmiziok} Kulaklığı kapalı: **${kulak}**
 
-<a:Revu11:900103903629160548> **Top 3 kategori sırası;**
+${Revuu} **Top 3 kategori sırası;**
 ${topCategory || 'Boş'}
 `)
 
