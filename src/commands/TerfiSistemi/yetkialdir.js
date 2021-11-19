34c42747263358c576939d0bed1504f090e2976d
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-11-19 16:47:59 +0300

    Star Atmayı Unutma

--- src/commands/TerfiSistemi/yetkialdir.js
+++ src/commands/TerfiSistemi/yetkialdir.js
@@ -44,7 +44,7 @@ module.exports = {
         await coin.findOneAndUpdate({ guildID: member.guild.id, userID: message.author.id }, { $inc: { coin: settings.yetkiCoin } }, { upsert: true });       
         msg.edit(`${member.toString()} üyesine başarıyla yetki aldırıldı! ${green}`).then(x => x.delete({timeout: 5000}))
         await yetkis.findOneAndUpdate({ guildID: message.guild.id, userID: message.author.id }, { $push: { yetkis: member.user.id } }, { upsert: true });
-       client.channels.cache.get(conf.yetkiLog).wsend(`${message.author} \`(${message.author.id}\` kişisi ${member} \`(${member.id})\` kişisini yetkiye aldı! <a:green:899337284481077298>`)
+       client.channels.cache.get(conf.yetkiLog).wsend(`${message.author} \`(${message.author.id}\` kişisi ${member} \`(${member.id})\` kişisini yetkiye aldı! ${green}`)
         member.roles.add(conf.yetkiRolleri)
       } else {
         msg.edit(`${member.toString()} üyesi, yetki aldırma teklifini reddetti! ${red}`).then(x => x.delete({timeout: 5000}))
