4f500f24e2063a9a2479952a7ba5e6020af6a9dd
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-12-06 20:31:14 +0300

    Star Atarsan Sevinirim Emek Var

--- src/events/userUpdate.js
+++ src/events/userUpdate.js
@@ -3,6 +3,8 @@ const client = global.client;
 const bannedTag = require("../schemas/bannedTag");
 const conf = require("../configs/sunucuayar.json");
 const settings = require("../configs/settings.json")
+const regstats = require("../schemas/registerStats");
+
 module.exports = async (oldUser, newUser) => {
     if (oldUser.bot || newUser.bot || (oldUser.username === newUser.username)) return;
     const guild = client.guilds.cache.get(settings.guildID);
@@ -12,7 +14,10 @@ module.exports = async (oldUser, newUser) => {
     const channel = guild.channels.cache.get(conf.ekipLogChannel);
     const kanal = guild.channels.cache.get(conf.chatChannel)
     if (oldUser.username.includes(conf.tag) && !newUser.username.includes(conf.tag)) {
-      if (conf.taglıAlım && !member.premiumSince) member.roles.set(conf.unregRoles);
+   const tagModedata = await regstats.findOne({ guildID: settings.guildID })
+    if (tagModedata && tagModedata.tagMode === true) {
+   if(!member.roles.cache.has(conf.vipRole) && !member.roles.cache.has(conf.boosterRolu)) return member.roles.set(conf.unregRoles);
+}
       else member.roles.remove(conf.ekipRolu);
       if (!channel) return;
       const embed = new MessageEmbed()
