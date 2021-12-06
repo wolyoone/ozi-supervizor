68e779ad204d3954f5285248932cb37883558cad
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-12-06 16:50:52 +0300

    Update cezabutton.js

--- src/commands/BotSahip/cezabutton.js
+++ src/commands/BotSahip/cezabutton.js
@@ -12,9 +12,9 @@ module.exports = {
   run: async (client, message, args) => {
     client.api.channels(message.channel.id).messages.post({ data: {"content":`${Jail} Aşağıda ki düğmelerden ceza listenizi, ceza puanını ve aktif cezanızın kalan süresini görüntüleyebilirsiniz.`,"components":[{"type":1,"components":[
 
-        {"type":2,"style":2,"custom_id":"cezapuan","label":"Ceza Puanı","emoji": { "id": "903565442206036019"}},
-        {"type":2,"style":3,"custom_id":"cezalarım","label":"Cezalarım","emoji": { "id": "903564842978402304"}},
-        {"type":2,"style":4,"custom_id":"kalanzaman","label":"Kalan Zamanım?","emoji": { "id": "903564832387760128"}}
+        {"type":2,"style":2,"custom_id":"cezapuan","label":"Ceza Puanı","emoji": { "id": "916734243328114718"}},
+        {"type":2,"style":3,"custom_id":"cezalarım","label":"Cezalarım","emoji": { "id": "915754688224321546"}},
+        {"type":2,"style":4,"custom_id":"kalanzaman","label":"Kalan Zamanım?","emoji": { "id": "915754687448379393"}}
 
         ]}]} })
   },
