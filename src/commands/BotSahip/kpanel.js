ff2fe0478552594b27ae18446afaf62b0589a9d5
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-12-06 16:51:44 +0300

    Update kpanel.js

--- src/commands/BotSahip/kpanel.js
+++ src/commands/BotSahip/kpanel.js
@@ -17,15 +17,15 @@ module.exports = {
                 "components": [{
                         "type": 1, "components": [{
                         "type": 3, "custom_id": "kpanel", "options": [
-                            { "label": "Sunucuya Katılma Tarihiniz", "value": "I", "emoji": { "id": "901441275381817426", "name": "ict_I" }, },
-                            { "label": "Üzerinde Bulunan Rollerin Listesi", "value": "II", "emoji": { "id": "901441275381817426", "name": "ict_II" }, },
-                            { "label": "Hesabınızın Açılış Tarihi", "value": "III", "emoji": { "id": "901441275381817426", "name": "ict_III" }, },
-                            { "label": "Toplam invite Bilgileri", "value": "IV", "emoji": { "id": "901441275381817426", "name": "ict_IV" }, },
-                            { "label": "Tekrar Kayıt Olma", "value": "V", "emoji": { "id": "901441275381817426", "name": "ict_V" }, },
-                            { "label": "Sunucu Bilgileri", "value": "VI", "emoji": { "id": "901441275381817426", "name": "ict_VI" }, },
-                            { "label": "İsim Bilgileri", "value": "VII", "emoji": { "id": "901441275381817426", "name": "ict_VII" }, },
-                            { "label": "Toplam Mesaj Bilgileri", "value": "VIII", "emoji": { "id": "901441275381817426", "name": "ict_VIII" }, },
-                            { "label": "Toplam Ses Bilgileri", "value": "IX", "emoji": { "id": "901441275381817426", "name": "ict_IX" }, }
+                            { "label": "Sunucuya Katılma Tarihiniz", "value": "I", "emoji": { "id": "915754675247120465", "name": "ict_I" }, },
+                            { "label": "Üzerinde Bulunan Rollerin Listesi", "value": "II", "emoji": { "id": "915754675247120465", "name": "ict_II" }, },
+                            { "label": "Hesabınızın Açılış Tarihi", "value": "III", "emoji": { "id": "915754675247120465", "name": "ict_III" }, },
+                            { "label": "Toplam invite Bilgileri", "value": "IV", "emoji": { "id": "915754675247120465", "name": "ict_IV" }, },
+                            { "label": "Tekrar Kayıt Olma", "value": "V", "emoji": { "id": "915754675247120465", "name": "ict_V" }, },
+                            { "label": "Sunucu Bilgileri", "value": "VI", "emoji": { "id": "915754675247120465", "name": "ict_VI" }, },
+                            { "label": "İsim Bilgileri", "value": "VII", "emoji": { "id": "915754675247120465", "name": "ict_VII" }, },
+                            { "label": "Toplam Mesaj Bilgileri", "value": "VIII", "emoji": { "id": "915754675247120465", "name": "ict_VIII" }, },
+                            { "label": "Toplam Ses Bilgileri", "value": "IX", "emoji": { "id": "915754675247120465", "name": "ict_IX" }, }
 
                         ], "placeholder": "Kullanıcı Menüsü", "min_values": 1, "max_values": 1
                     }],
