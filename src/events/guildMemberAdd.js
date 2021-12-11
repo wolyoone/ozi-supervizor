15326f5fe9eb93c84853518f8df0d8033e564e81
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-12-12 01:16:07 +0300

    Emeğe Saygı Star Atmayı Unutma

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -36,7 +36,8 @@ module.exports = async (member) => {
     if (tagModedata && tagModedata.tagMode === false) {
       if (otoreg) {
         if (member.manageable) await member.roles.set(otoreg.roleID)
-        member.setNickname(`${ayar.tag} ${otoreg.name} ' ${otoreg.age}`);
+        member.setNickname(`${ayar.tag} ${otoreg.name}`);
+       if(ayar.chatChannel && client.channels.cache.has(ayar.chatChannel)) client.channels.cache.get(ayar.chatChannel).send(`Aramıza hoşgeldin **${member}**! Sunucumuzda daha önceden kayıtın bulunduğu için direkt içeriye alındınız. Kuralları okumayı unutma!`).then(x => x.delete({timeout: 10000})) 
       }
 }
 
